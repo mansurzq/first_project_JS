0e66b54ec96c29f84a1d55c6a25ec62bc87bd506
Mansur <mxnsur@duck.com>
2024-04-30 11:34:25 +0500

    automatiozation with loops and added more questions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,12 +10,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const film1 = prompt('Один из последних просмотренных фильмов?', ''),
-      rating1 = +prompt('На сколько оцените его?', ''),
-      film2 = prompt('Один из последних просмотренных фильмов?', ''),
-      rating2 = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const film = prompt('Один из последних просмотренных фильмов?', ''),
+          rating = +prompt('На сколько оцените его?', '');
+    
+    if (film != '' && rating != '' && film != null && rating != null && film.length < 50) {
+        personalMovieDB.movies[film] = rating
+    } else {
+        console.log('error!');
+        i--;
+    }
+}
 
-personalMovieDB.movies[film1] = rating1;
-personalMovieDB.movies[film2] = rating2;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
